0af75996d3802624bcd3a61989b6f6bd8b8add26
FelixPriori <felixriouxsabourin@gmail.com>
2020-03-30 18:39:00 +0000

    Implemented exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,34 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+store1 = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true,
+)
+
+store2 = Store.create(
+  name: "Burnaby",
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true,
+)
+
+store3 = Store.create(
+  name: "Burnaby",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false,
+)
+
+puts Store.count
+
+=begin
+
+  CREATE TABLE "stores" ("id" serial primary key, "name" character varying, "annual_revenue" integer, "mens_apparel" boolean, "womens_apparel" boolean, "created_at" timestamp NOT NULL, "updated_at" timestamp NOT NULL) 
+
+  CREATE TABLE "employees" ("id" serial primary key, "store_id" integer, "first_name" character varying, "last_name" character varying, "hourly_rate" integer, "created_at" timestamp NOT NULL, "updated_at" timestamp NOT NULL) 
+
+=end
