27c552e4953d6ca77e5eb77f2cd83d0d20a83429
FelixPriori <felixriouxsabourin@gmail.com>
2020-03-30 18:45:25 +0000

    implemented exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,18 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.name = 'Chicken'
+
+puts @store1.name
+puts @store2.name
+
+=begin
+
+  CREATE TABLE "stores" ("id" serial primary key, "name" character varying, "annual_revenue" integer, "mens_apparel" boolean, "womens_apparel" boolean, "created_at" timestamp NOT NULL, "updated_at" timestamp NOT NULL) 
+
+  CREATE TABLE "employees" ("id" serial primary key, "store_id" integer, "first_name" character varying, "last_name" character varying, "hourly_rate" integer, "created_at" timestamp NOT NULL, "updated_at" timestamp NOT NULL) 
+
+=end
